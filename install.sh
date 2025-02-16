cc05284779dcbbc7c1c0b31603fc034e4c0499b1
kulkarn1 <nachiket.kulkarni@autodesk.com>
2025-02-16 16:46:59 +0530

    Some changes

--- install.sh
+++ install.sh
@@ -5,14 +5,15 @@ docker build -t fastapi-app:latest -f Dockerfile_backend .
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl port-forward svc/argocd-server -n argocd 8080:443
+# Check if the argocd-server pod is in Running state
+while [[ $(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o jsonpath="{.items[0].status.phase}") != "Running" ]]; do
+    echo "Waiting for argocd-server pod to be in Running state..."
+    sleep 5
+done
 
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl port-forward svc/argocd-server -n argocd 8080:443
 
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-argocd login localhost:8080 &
 
 kubectl apply -f k8s/argocd-app.yaml
 
